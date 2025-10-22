e0511b76cce886a7c57da9a14c418a34105c636e
mini0192 <parkgw9071@gmail.com>
2025-10-22 17:02:25 +0900

    Docs: delete anntation in the DocConfig

--- src/main/java/com/matching/config/DocConfig.java
+++ src/main/java/com/matching/config/DocConfig.java
@@ -6,10 +6,10 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @SecurityScheme(
-        name = "bearerAuth",               // 이름은 나중에 @SecurityRequirement에서 참조
-        type = SecuritySchemeType.HTTP,    // HTTP 인증 방식
-        bearerFormat = "JWT",              // JWT 형식 명시
-        scheme = "bearer"                  // Bearer 토큰 방식
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        scheme = "bearer"
 )
 public class DocConfig {
 }
