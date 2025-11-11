906f283d587beb806d19ee7bead798f1ce0af45e
mini0192 <parkgw9071@gmail.com>
2025-11-11 22:37:45 +0900

    Feat: random

--- src/main/java/com/matching/core/test/infra/GrammarTestRepository.java
+++ src/main/java/com/matching/core/test/infra/GrammarTestRepository.java
@@ -3,10 +3,13 @@ package com.matching.core.test.infra;
 import com.matching.core.test.view.Level;
 import com.matching.core.test.view.TestType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.UUID;
 
 public interface GrammarTestRepository extends JpaRepository<GrammarTest, UUID> {
-    List<GrammarTest> findByLevelAndType(Level level, TestType type);
+    @Query(value = "SELECT gt FROM GrammarTest gt WHERE gt.level = :level AND gt.type = :type ORDER BY RANDOM()")
+    List<GrammarTest> findByLevelAndType(@Param("level") Level level, @Param("type") TestType type);
 }
